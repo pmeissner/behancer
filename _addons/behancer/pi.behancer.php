244bf533dd31629ece786b5577db8e9ed1b2f826
Philip Meissner <philipmeissnerdesign@gmail.com>
2014-02-13 13:15:23 -0500

    adding two functions

--- _addons/behancer/pi.behancer.php
+++ _addons/behancer/pi.behancer.php
@@ -4,12 +4,13 @@ class Plugin_behancer extends Plugin {
 
 	var $meta = array(
 		'name'        => 'Behancer',
-		'version'     => '1',
+		'version'     => '1.1',
 		'author'      => 'Philip Meissner',
 		'author_url'  => 'http://lou.pe'
 	);
 
-	public function index() {
+
+	public function project() {
 
 		$beConfig = array();
 		$beConfig['client_id'] = $this->fetch('client_id', null, null, false, false);
@@ -17,12 +18,12 @@ class Plugin_behancer extends Plugin {
 
 		$project_id = $this->fetchParam('id');
 
-		$yesterday = time() - 60 * 60 * 24;
+		$yesterday = time() - 1;
 		$this->cache->purgeFromBefore($yesterday);
 
-		if ($this->cache->exists($project_id . '_content.yaml')) {
+		if ($this->cache->exists($project_id . '_project.yaml')) {
 
-			$output = $this->cache->getYAML($project_id . '_content.yaml');
+			$output = $this->cache->getYAML($project_id . '_project.yaml');
 
 		} else {
 
@@ -34,12 +35,47 @@ class Plugin_behancer extends Plugin {
 			$encoded = json_encode($project);
 			$decoded = json_decode($encoded, true);
 
-			$this->cache->putYAML($project_id . '_content.yaml', $decoded);
-			$output = $this->cache->getYAML($project_id . '_content.yaml');
+			$this->cache->putYAML($project_id . '_project.yaml', $decoded);
+			$output = $this->cache->getYAML($project_id . '_project.yaml');
 
 		};
 
 		return $output;
 
 	}
+
+	public function listing() {
+
+		$beConfig = array();
+		$beConfig['client_id'] = $this->fetch('client_id', null, null, false, false);
+		$beConfig['client_secret'] = $this->fetch('client_secret', null, null, false, false);
+
+		$user_id = $this->fetchParam('user');
+
+		$yesterday = time() - 60 * 60 * 24;
+		$this->cache->purgeFromBefore($yesterday);
+
+		if ($this->cache->exists($user_id . '_user.yaml')) {
+
+			$output = $this->cache->getYAML($user_id . '_user.yaml');
+
+		} else {
+
+			require_once( 'lib/Be/Api.php' );
+
+			$api = new Be_Api( $beConfig['client_id'], $beConfig['client_secret'] );
+			$listing = $api->getUserProjects($user_id);
+
+			$encoded = "{\"listing\":" . json_encode($listing) . "}";
+			$decoded = json_decode($encoded, true);
+
+			$this->cache->putYAML($user_id . '_user.yaml', $decoded);
+			$output = $this->cache->getYAML($user_id . '_user.yaml');
+
+		};
+
+		return $output;
+
+	}
+
 }
